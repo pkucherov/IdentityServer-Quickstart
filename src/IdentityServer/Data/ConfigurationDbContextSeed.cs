6835c9cf22849394532b6aa580210e23fb60c620
Pavel Kucherov <26567338+pkucherov@users.noreply.github.com>
2020-11-22 17:32:24 +0100

    Update ConfigurationDbContextSeed

--- src/IdentityServer/Data/ConfigurationDbContextSeed.cs
+++ src/IdentityServer/Data/ConfigurationDbContextSeed.cs
@@ -28,27 +28,6 @@ namespace IdentityServer.Data
                 }
                 await context.SaveChangesAsync();
             }
-            // Checking always for old redirects to fix existing deployments
-            // to use new swagger-ui redirect uri as of v3.0.0
-            // There should be no problem for new ones
-            // ref: https://github.com/dotnet-architecture/eShopOnContainers/issues/586
-            else
-            {
-                List<ClientRedirectUri> oldRedirects = (await context.Clients.Include(c => c.RedirectUris).ToListAsync())
-                    .SelectMany(c => c.RedirectUris)
-                    .Where(ru => ru.RedirectUri.EndsWith("/o2c.html"))
-                    .ToList();
-
-                if (oldRedirects.Any())
-                {
-                    foreach (var ru in oldRedirects)
-                    {
-                        ru.RedirectUri = ru.RedirectUri.Replace("/o2c.html", "/oauth2-redirect.html");
-                        context.Update(ru.Client);
-                    }
-                    await context.SaveChangesAsync();
-                }
-            }
 
             if (!context.IdentityResources.Any())
             {
