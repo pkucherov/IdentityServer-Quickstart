dd6d373d98bad7953f4d36164c217eb3996ecb6e
Pavel Kucherov <26567338+pkucherov@users.noreply.github.com>
2020-11-22 21:27:42 +0100

    Implement simple client

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Client
@@ -66,7 +67,7 @@ namespace Client
 
             {
                 var client = new HttpClient();
-                var tokenResponse2 = await client.RequestPasswordTokenAsync(new PasswordTokenRequest
+                var tokenResponse2 = await client.RequestPasswordTokenAsync(new IdentityModel.Client.PasswordTokenRequest
                 {
                     Address = disco.TokenEndpoint,
                     Password = "Pass@word1",
@@ -109,7 +110,7 @@ namespace Client
             {
                 var client4 = new HttpClient();
                 //reference token
-                var tokenResponse4 = await client4.RequestPasswordTokenAsync(new PasswordTokenRequest
+                var tokenResponse4 = await client4.RequestPasswordTokenAsync(new IdentityModel.Client.PasswordTokenRequest
                 {
                     Address = disco.TokenEndpoint,
                     Password = "Pass@word1",
@@ -142,6 +143,151 @@ namespace Client
                 client4.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken4);
                 var identityGet4 = await client4.GetStringAsync(baseAddress + "/identity");
                 Console.WriteLine($"GET: {identityGet4}");
+            }
+            {
+                var client5 = new HttpClient();
+                //reference token
+                var tokenResponse4 = TestClient.RequestPasswordToken(client5,new PasswordTokenRequest
+                {
+                    Address = disco.TokenEndpoint,
+                    Password = "Pass@word1",
+                    UserName = "demouser@microsoft.com",
+                    Scope = "api1",
+                    ClientId = "ro.client2",
+                    ClientSecret = "secret"
+                });
+
+                if (!tokenResponse4.IsSuccessStatusCode)
+                {
+                    Console.WriteLine(tokenResponse4);
+                    return;
+                }
+
+                Console.WriteLine(tokenResponse4);
+                Console.WriteLine("\n\n");
+
+                /*
+                var accessToken4 = tokenResponse4.Json["access_token"].ToString();
+                // WeatherForecast
+                //       client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
+                var weatherGet4 = await client5.GetStringAsync(baseAddress + "/WeatherForecast");
+
+
+                HttpRequestMessage request4 = new HttpRequestMessage(HttpMethod.Get, baseAddress + "/identity");
+                request4.Headers.Add("Authorization", new List<string>() { "Bearer " + accessToken4 });
+                request4.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken4);
+                var resp4 = await client5.SendAsync(request4);
+
+                client5.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken4);
+                var identityGet4 = await client5.GetStringAsync(baseAddress + "/identity");
+                Console.WriteLine($"GET: {identityGet4}");
+                */
+            }
+        }
+    }
+
+    class TestClient
+    {
+        public static HttpResponseMessage RequestPasswordToken(HttpMessageInvoker client, PasswordTokenRequest request)
+        {
+            var clone = request;
+
+            clone.Parameters.Add(OidcConstants.TokenRequest.GrantType, OidcConstants.GrantTypes.Password);
+            clone.Parameters.Add(OidcConstants.TokenRequest.UserName, request.UserName);
+            clone.Parameters.Add(OidcConstants.TokenRequest.Password, request.Password);
+            clone.Parameters.Add(OidcConstants.TokenRequest.Scope, request.Scope);
+
+            return RequestToken(client, clone);
+        }
+
+        static HttpResponseMessage RequestToken(HttpMessageInvoker client, PasswordTokenRequest request)
+        {
+            request.Prepare();
+            request.Method = HttpMethod.Post;
+
+            HttpResponseMessage response = null;
+            try
+            {
+                CancellationToken cancellationToken = default;
+                response = client.Send(request, cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                
+            }
+
+            return response;
+        }
+    }
+
+    
+    public class PasswordTokenRequest : HttpRequestMessage
+    {
+        public string Address { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public ClientAssertion ClientAssertion { get; set; }
+        public ClientCredentialStyle ClientCredentialStyle { get; set; } = ClientCredentialStyle.PostBody;
+        public BasicAuthenticationHeaderStyle AuthorizationHeaderStyle { get; set; } = BasicAuthenticationHeaderStyle.Rfc6749;
+        public IDictionary<string, string> Parameters { get; set; } = new Dictionary<string, string>();
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Scope { get; set; }
+        public string GrantType { get; set; }
+
+        public PasswordTokenRequest()
+        {
+            Headers.Accept.Clear();
+            Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public void Prepare()
+        {
+            if (ClientId != null)
+            {
+                if (ClientCredentialStyle == ClientCredentialStyle.AuthorizationHeader)
+                {
+                    if (AuthorizationHeaderStyle == BasicAuthenticationHeaderStyle.Rfc6749)
+                    {
+                        this.SetBasicAuthenticationOAuth(ClientId, ClientSecret ?? "");
+                    }
+                    else if (AuthorizationHeaderStyle == BasicAuthenticationHeaderStyle.Rfc2617)
+                    {
+                        this.SetBasicAuthentication(ClientId, ClientSecret ?? "");
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException("Unsupported basic authentication header style");
+                    }
+                }
+                else if (ClientCredentialStyle == ClientCredentialStyle.PostBody)
+                {
+                    Parameters.Add(OidcConstants.TokenRequest.ClientId, ClientId);
+                    Parameters.Add(OidcConstants.TokenRequest.ClientSecret, ClientSecret);
+                }
+                else
+                {
+                    throw new InvalidOperationException("Unsupported client credential style");
+                }
+            }
+
+            if (ClientAssertion != null)
+            {
+                if (ClientAssertion.Type != null && ClientAssertion.Value != null)
+                {
+                    Parameters.Add(OidcConstants.TokenRequest.ClientAssertionType, ClientAssertion.Type);
+                    Parameters.Add(OidcConstants.TokenRequest.ClientAssertion, ClientAssertion.Value);
+                }
+            }
+
+            if (Address!=null)
+            {
+                RequestUri = new Uri(Address);
+            }
+
+            if (Parameters!=null)
+            {
+                Content = new FormUrlEncodedContent(Parameters);
             }
         }
     }
