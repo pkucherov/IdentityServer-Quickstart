9267c008d5ecfecc3667873bdbaf86f5a50713f5
Pavel Kucherov <26567338+pkucherov@users.noreply.github.com>
2020-11-22 17:30:51 +0100

    Add ApplicationDbContextSeed and ConfigurationDbContextSeed

---
+++ src/IdentityServer/Data/ApplicationDbContextSeed.cs
@@ -0,0 +1,77 @@
+﻿using IdentityServer.Model;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Data
+{
+
+
+    public class ApplicationDbContextSeed
+    {
+        private readonly IPasswordHasher<ApplicationUser> _passwordHasher = new PasswordHasher<ApplicationUser>();
+
+        public async Task SeedAsync(ApplicationDbContext context,IWebHostEnvironment env,
+            ILogger<ApplicationDbContextSeed> logger, int? retry = 0)
+        {
+            int retryForAvaiability = retry.Value;
+
+            try
+            {
+                var contentRootPath = env.ContentRootPath;
+                var webroot = env.WebRootPath;
+
+                if (!context.Users.Any())
+                {
+                    context.Users.AddRange(GetDefaultUser());
+
+                    await context.SaveChangesAsync();
+                }
+                                
+            }
+            catch (Exception ex)
+            {
+                if (retryForAvaiability < 10)
+                {
+                    retryForAvaiability++;
+                    
+                    logger.LogError(ex, "EXCEPTION ERROR while migrating {DbContextName}", nameof(ApplicationDbContext));
+
+                    await SeedAsync(context,env,logger,retryForAvaiability);
+                }
+            }
+        }
+
+        private IEnumerable<ApplicationUser> GetDefaultUser()
+        {
+            var user =
+            new ApplicationUser()
+            {
+                Email = "demouser@microsoft.com",
+                Id = Guid.NewGuid().ToString(),
+                LastName = "DemoLastName",
+                Name = "DemoUser",
+                PhoneNumber = "1234567890",
+                UserName = "demouser@microsoft.com",
+                NormalizedEmail = "DEMOUSER@MICROSOFT.COM",
+                NormalizedUserName = "DEMOUSER@MICROSOFT.COM",
+                SecurityStamp = Guid.NewGuid().ToString("D"),
+            };
+
+            user.PasswordHash = _passwordHasher.HashPassword(user, "Pass@word1");
+
+            return new List<ApplicationUser>()
+            {
+                user
+            };
+        }
+    }
+}
